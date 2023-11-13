8ee6807a243d964fae554090a95fc11bf699c800
Lawrencia628 <135554673+Lawrencia628@users.noreply.github.com>
2023-11-13 22:58:54 +0000

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -2,10 +2,10 @@
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * @list: List of all arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
+ * @flags: Calculates the active flags
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
